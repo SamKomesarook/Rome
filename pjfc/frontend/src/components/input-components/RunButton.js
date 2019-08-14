54fedbabe7ce042e1756edabca7f5866aa7f6e53
Hongyi Zheng <s3649928@student.rmit.edu.au>
2019-08-14 14:01:18 +1000

    Basic input components
    Slider,textarea and button components with very few styling

---
+++ pjfc/frontend/src/components/input-components/RunButton.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+ export class RunButton extends Component {
+  render() {
+    return (
+		<div class="form-group shadow-textarea">
+            <button type="button" class="btn btn-primary">Run</button>
+        </div>
+    );
+  }
+}
+
+ export default RunButton;
