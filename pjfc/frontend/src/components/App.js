8d8db67f9b14a3e829b6c53da0b1ab617be811ac
nil <nil>
2019-08-14 19:50:12 +1000

    add memory layout

--- pjfc/frontend/src/components/App.js
+++ pjfc/frontend/src/components/App.js
@@ -2,15 +2,59 @@ import React, { Component, Fragment } from "react";
 import ReactDOM from "react-dom";
 
 import Header from "./layout/Header";
+import {Memory, USBMemory, NetMemory} from "./elements/Memory";
 
 export class App extends Component {
   render() {
+
     return (
       <Fragment>
-        <Header />
+      <div>
+        <Memory selected={true}/>
+        <Memory selected={false}/>
+        <Memory selected={false}/>
+      </div>
+      <div>
+        <Memory selected={false}/>
+        <Memory selected={false}/>
+        <Memory selected={false}/>
+      </div>
+      <div>
+        <Memory selected={false}/>
+        <Memory selected={false}/>
+        <Memory selected={false}/>
+      </div>
+      <div>
+        <Memory selected={false}/>
+        <Memory selected={false}/>
+        <Memory selected={false}/>
+      </div>
+      <div>
+        <Memory selected={false}/>
+        <USBMemory selected={false}/>
+        <NetMemory selected={false}/>
+      </div>
       </Fragment>
     );
   }
 }
+
+/*
+<Memory selected={true}/>
+<Memory selected={false}/>
+<Memory selected={false}/>
+{"\n"}
+<Memory selected={false}/>
+<Memory selected={false}/>
+<Memory selected={false}/>
+{"\n"}
+<Memory selected={false}/>
+<Memory selected={false}/>
+<Memory selected={false}/>
+{"\n"}
+<Memory selected={false}/>
+<NetMemory selected={false}/>
+<USBMemory selected={false}/>
+*/
 
 ReactDOM.render(<App />, document.getElementById("app"));
--- pjfc/frontend/src/components/elements/Memory.js
+++ pjfc/frontend/src/components/elements/Memory.js
@@ -18,11 +18,11 @@ export class NetMemory extends Component{
     render() {
         if (this.props.selected){
             return (
-              <div className="shadow-lg p-3 mb-5 bg-brown rounded">Larger shadow</div>
+              <div className="shadow-lg p-3 mb-5 bg-brown rounded border border-info">Larger shadow</div>
             );
         }else{
             return (
-              <div className="shadow-none p-3 mb-5 bg-brown rounded">Smaller shadow</div>
+              <div className="shadow-none p-3 mb-5 bg-brown rounded border border-info">Smaller shadow</div>
             );
         }
     }
@@ -32,11 +32,11 @@ export class USBMemory extends Component{
     render() {
         if (this.props.selected){
             return (
-              <div className="shadow-lg p-3 mb-5 bg-blue rounded">Larger shadow</div>
+              <div className="shadow-lg p-3 mb-5 bg-blue rounded border border-dark">Larger shadow</div>
             );
         }else{
             return (
-              <div className="shadow-none p-3 mb-5 bg-blue rounded">Smaller shadow</div>
+              <div className="shadow-none p-3 mb-5 bg-blue rounded border border-dark">Smaller shadow</div>
             );
         }
     }
