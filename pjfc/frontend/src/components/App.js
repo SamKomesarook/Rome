dd2d7d3d58a7d99316da4197dd07377a74fc1275
liam619 <s3697652@student.rmit.edu.au>
2019-08-15 08:30:43 +1000

    Added prompt new window in a button

--- pjfc/frontend/src/components/App.js
+++ pjfc/frontend/src/components/App.js
@@ -1,10 +1,44 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import User_Input_Window from './layout/User_Input_Window';
 
 export class App extends Component {
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            showWindowPortal: false
+        };
+
+        this.toggleWindow = this.toggleWindowPortal.bind(this);
+        this.closeWindow = this.closeWindowPortal.bind(this);
+    }
+
+    componentDidMount() {
+        window.addEventListener('beforeunload', () => {
+            this.closeWindowPortal();
+        });
+    }
+
+    toggleWindowPortal() {
+        this.setState(state => ({
+            ...state,
+            showWindowPortal: !state.showWindowPortal,
+        }))
+    }
+
+    closeWindowPortal() {
+        this.setState({ showWindowPortal: false})
+    }
+
     render() {
         return (
-            <h1>React App</h1>
+            <div>
+                <h1>React App</h1>
+                <button onClick={() => this.toggleWindowPortal()}>Open test</button>
+                {this.state.showWindowPortal && <User_Input_Window closeWindowPortal={this.closeWindowPortal} />}
+            </div>
         )
     }
 }
