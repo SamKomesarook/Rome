98718ed9ceeca1ed469f29c116cb384ec183fb35
nil <nil>
2019-08-20 11:12:26 +1000

    add grammar and antlr4-gen files

---
+++ pjfc/backend/lang/Rome.g4
@@ -0,0 +1,64 @@
+grammar Rome;
+
+options {
+	language = Python;
+}
+
+START:'start';
+END:'end';
+
+SET:'set';
+NUM:'numbers';
+LET:'letters';
+
+FREE:'free';
+MEM:'memory';
+READ:'read';
+WRITE:'write';
+
+IF:'if';
+IS:'is';
+NOT:'not';
+EQL:'equal';
+LESS:'less';
+GRE:'greater';
+//AND:'and';
+//OR:'or';
+
+MOVE:'move';
+RIGHT:'right';
+LEFT:'left';
+
+LPAR:'(';
+RPAR:')';
+LSQ:'[';
+RSQ:']';
+
+NEWLINE:'\n';
+
+NUMBER
+   : ('0' .. '9') +
+   ;
+
+intargs
+	: NUMBER
+	| MEM LPAR intargs RPAR
+	;
+
+STRLIT : '"' ~ ["\r\n]* '"';
+
+r : START NEWLINE expressions* END NEWLINE?;
+
+expressions : expression NEWLINE;
+
+expression
+	: SET LPAR (NUM | LET) RPAR # Set
+	| FREE # Free
+	| READ # Read
+	| MOVE LPAR (RIGHT | LEFT) RPAR # Move
+	| IF LPAR (IS|NOT) (LESS|GRE|EQL) (intargs|STRLIT) RPAR LSQ NEWLINE expressions* RSQ # If //((AND|OR) (IS|NOT) (LESS|GRE|EQL) (intargs|STRLIT))*
+	| LSQ NEWLINE expressions+ RSQ LSQ intargs RSQ # Loop
+	| WRITE LPAR (intargs|STRLIT) RPAR # Write
+	;
+
+IDENTIFIER : [a-zA-Z]+;
---
+++ pjfc/backend/lang/RomeListener.py
@@ -0,0 +1,100 @@
+# Generated from Rome.g4 by ANTLR 4.7.2
+from antlr4 import *
+if __name__ is not None and "." in __name__:
+    from .RomeParser import RomeParser
+else:
+    from RomeParser import RomeParser
+
+# This class defines a complete listener for a parse tree produced by RomeParser.
+class RomeListener(ParseTreeListener):
+
+    # Enter a parse tree produced by RomeParser#intargs.
+    def enterIntargs(self, ctx:RomeParser.IntargsContext):
+        pass
+
+    # Exit a parse tree produced by RomeParser#intargs.
+    def exitIntargs(self, ctx:RomeParser.IntargsContext):
+        pass
+
+
+    # Enter a parse tree produced by RomeParser#r.
+    def enterR(self, ctx:RomeParser.RContext):
+        pass
+
+    # Exit a parse tree produced by RomeParser#r.
+    def exitR(self, ctx:RomeParser.RContext):
+        pass
+
+
+    # Enter a parse tree produced by RomeParser#expressions.
+    def enterExpressions(self, ctx:RomeParser.ExpressionsContext):
+        pass
+
+    # Exit a parse tree produced by RomeParser#expressions.
+    def exitExpressions(self, ctx:RomeParser.ExpressionsContext):
+        pass
+
+
+    # Enter a parse tree produced by RomeParser#Set.
+    def enterSet(self, ctx:RomeParser.SetContext):
+        pass
+
+    # Exit a parse tree produced by RomeParser#Set.
+    def exitSet(self, ctx:RomeParser.SetContext):
+        pass
+
+
+    # Enter a parse tree produced by RomeParser#Free.
+    def enterFree(self, ctx:RomeParser.FreeContext):
+        pass
+
+    # Exit a parse tree produced by RomeParser#Free.
+    def exitFree(self, ctx:RomeParser.FreeContext):
+        pass
+
+
+    # Enter a parse tree produced by RomeParser#Read.
+    def enterRead(self, ctx:RomeParser.ReadContext):
+        pass
+
+    # Exit a parse tree produced by RomeParser#Read.
+    def exitRead(self, ctx:RomeParser.ReadContext):
+        pass
+
+
+    # Enter a parse tree produced by RomeParser#Move.
+    def enterMove(self, ctx:RomeParser.MoveContext):
+        pass
+
+    # Exit a parse tree produced by RomeParser#Move.
+    def exitMove(self, ctx:RomeParser.MoveContext):
+        pass
+
+
+    # Enter a parse tree produced by RomeParser#If.
+    def enterIf(self, ctx:RomeParser.IfContext):
+        pass
+
+    # Exit a parse tree produced by RomeParser#If.
+    def exitIf(self, ctx:RomeParser.IfContext):
+        pass
+
+
+    # Enter a parse tree produced by RomeParser#Loop.
+    def enterLoop(self, ctx:RomeParser.LoopContext):
+        pass
+
+    # Exit a parse tree produced by RomeParser#Loop.
+    def exitLoop(self, ctx:RomeParser.LoopContext):
+        pass
+
+
+    # Enter a parse tree produced by RomeParser#Write.
+    def enterWrite(self, ctx:RomeParser.WriteContext):
+        pass
+
+    # Exit a parse tree produced by RomeParser#Write.
+    def exitWrite(self, ctx:RomeParser.WriteContext):
+        pass
+
+
