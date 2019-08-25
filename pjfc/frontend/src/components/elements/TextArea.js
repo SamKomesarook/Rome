e1474fb5a0677a6cc4103cfa104891a99209bf05
Hongyi Zheng <s3649928@student.rmit.edu.au>
2019-08-25 20:49:00 +1000

    add line number
    add line number to the text area with css

--- pjfc/frontend/src/components/elements/TextArea.js
+++ pjfc/frontend/src/components/elements/TextArea.js
@@ -1,13 +1,28 @@
 import React, { Component } from "react";
 
 export class TextArea extends Component {
+  
+  
+  
+  
   render() {
+    const mystyle = {
+      background: "url(http://i.imgur.com/2cOaJ.png)",
+      backgroundAttachment: "local",
+      backgroundRepeat: "no-repeat",
+      paddingLeft: "35px",
+      paddingTop: "10px",
+      borderColor:"#ccc"
+    };
+  
+
     return (
       <div className="wrapper">
         <div className="form-group shadow-textarea">
           <textarea
             data-tip data-for='TextAreaTips'
             className="form-control rounded-0 z-depth-1"
+            style={mystyle}
             cols="50"
             id="codingArea"
             rows="20"
