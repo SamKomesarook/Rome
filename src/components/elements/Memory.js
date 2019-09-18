409853d0da8bdb26ceec8de4dccd96ecaf9f09c6
liam619 <s3697652@student.rmit.edu.au>
2019-09-18 14:20:58 +1000

    Create different border style for set animation (letters & numbers)

--- src/components/elements/Memory.js
+++ src/components/elements/Memory.js
@@ -8,18 +8,19 @@ export class Memory extends Component {
     if (this.props.selected) {
       return (
         <div
-          className="shadow-lg p-3 mb-5 bg-white rounded"
+          className="shadow-lg p-3 mb-5 bg-white rounded border border-danger"
           id={this.props.id}
           data-tip
           data-for="MemoryTips"
         >
           {this.props.content}
+          
         </div>
       );
     } else {
       return (
         <div
-          className="shadow-none p-3 mb-5 bg-white rounded"
+          className={"p-3 mb-5 bg-white rounded " + ((this.props.contentType == 'letters')? "lightGray" : "lightBlue")}
           id={this.props.id}
           data-tip
           data-for="MemoryTips"
@@ -50,7 +51,7 @@ export class NetMemory extends Component {
     } else {
       return (
         <div
-          className="shadow-none p-3 mb-5 bg-brown rounded border border-info"
+          className={"p-3 mb-5 bg-white bg-brown rounded " + ((this.props.contentType == 'letters')? "lightGray" : "lightBlue")}
           id={this.props.id}
           data-tip
           data-for="NetMemoryTips"
@@ -70,7 +71,7 @@ export class USBMemory extends Component {
     if (this.props.selected) {
       return (
         <div
-          className="shadow-lg p-3 mb-5 bg-blue rounded border border-dark"
+          className="shadow-lg p-3 mb-5 bg-blue rounded border border-warning"
           id={this.props.id}
           data-tip
           data-for="USBMemoryTips"
@@ -81,7 +82,7 @@ export class USBMemory extends Component {
     } else {
       return (
         <div
-          className="shadow-none p-3 mb-5 bg-blue rounded border border-dark"
+          className={"p-3 mb-5 bg-blue rounded " + ((this.props.contentType == 'letters')? "lightGray" : "lightBlue")}
           id={this.props.id}
           data-tip
           data-for="USBMemoryTips"
