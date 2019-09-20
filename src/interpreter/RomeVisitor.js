11f46bbb15cd3d3dc99f7a9b95bebaf5c079f3aa
BoxHezi <547181293@qq.com>
2019-09-20 16:55:51 +1000

    make interpreter using visitor

--- src/interpreter/RomeVisitor.js
+++ src/interpreter/RomeVisitor.js
@@ -1,8 +1,12 @@
-
 import { RomeVisitor } from "../lang/RomeVisitor";
-import MemoryBlock from "../entity/MemoryBlock";
 
 // Visit a parse tree produced by RomeParser#Num.
 RomeVisitor.prototype.visitNum = function(ctx) {
   return this.visitChildren(ctx);
 };
+
+RomeVisitor.prototype.visitMove = function(ctx) {
+  console.log("Visit Move!");
+};
+
+export default RomeVisitor;
