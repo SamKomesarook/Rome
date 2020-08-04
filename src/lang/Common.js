3d1faeeacca06abf98461b3e01f9e424f557b507
Ton Binh On <nick.on437@gmail.com>
2020-08-04 10:58:02 +1000

    Auto fix files directly under lang and Rome folder

--- src/lang/Common.js
+++ src/lang/Common.js
@@ -1,10 +1,11 @@
-import { RVisitor } from './RomeVisitor'
-import { MVisitor } from './MachineVisitor'
-var antlr4 = require("antlr4");
+import { RVisitor } from './RomeVisitor';
+import { MVisitor } from './MachineVisitor';
+
+const antlr4 = require('antlr4');
 
 function processInstrs(display, setDisplay) {
-    //var delayCount = 0;
-    /*
+  // var delayCount = 0;
+  /*
     if (display.commands.length == 0) {
         break
     }
@@ -20,46 +21,48 @@ function processInstrs(display, setDisplay) {
         }
     }
     */
-    while (true) {
-        if (display.commands.length === 0 || display.errors) {
-            break
-        }
-        var instr = display.commands[0]
-        display.commands.splice(0, 1)
-        if (instr.children[0].constructor.name === "KreadContext" || instr.children[0].constructor.name === "ReadContext") {
-            if(!display.importIO){
-                var errors = new ErrorReporter(display)
-                errors.generalError("Unknown function 'k_read'")
-                return
-            }
-            setDisplay(display => ({
-                ...display,
-                reading: true
-            }))
-            break
-        } else {
-            instr.accept(display.machine ? new MVisitor(setDisplay, display) : new RVisitor(setDisplay, display))
-        }
+  while (true) {
+    if (display.commands.length === 0 || display.errors) {
+      break;
     }
-    return true
+    const instr = display.commands[0];
+    display.commands.splice(0, 1);
+    if (instr.children[0].constructor.name === 'KreadContext' || instr.children[0].constructor.name === 'ReadContext') {
+      if (!display.importIO) {
+        const errors = new ErrorReporter(display);
+        errors.generalError("Unknown function 'k_read'");
+        return;
+      }
+      setDisplay((display) => ({
+        ...display,
+        reading: true,
+      }));
+      break;
+    } else {
+      instr.accept(display.machine ? new MVisitor(setDisplay, display) : new RVisitor(setDisplay, display));
+    }
+  }
+  return true;
 }
 
 class ErrorReporter extends antlr4.error.ErrorListener {
-    constructor(display) {
-        super();
-        this.display = display;
-    }
-    syntaxError(recognizer, offendingSymbol, line, column, msg, e) {
-        this.display.output = this.display.output.concat(msg + "\n")
-        this.display.errors = true
-    }
-    generalError(msg){
-        this.display.output = this.display.output.concat(msg + "\n")
-        this.display.errors = true
-    }
+  constructor(display) {
+    super();
+    this.display = display;
+  }
+
+  syntaxError(recognizer, offendingSymbol, line, column, msg, e) {
+    this.display.output = this.display.output.concat(`${msg}\n`);
+    this.display.errors = true;
+  }
+
+  generalError(msg) {
+    this.display.output = this.display.output.concat(`${msg}\n`);
+    this.display.errors = true;
+  }
 }
 
 export {
-    processInstrs,
-    ErrorReporter
+  processInstrs,
+  ErrorReporter,
 };
