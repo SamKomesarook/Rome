146f0148ff5b175e2341e01c48e6e3b53ab73749
czzlyin <62701549+czzlyin@users.noreply.github.com>
2020-08-24 16:43:52 +1000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import InputField from './components/elements/InputField';
 import SubmitButton from './components/elements/SubmitButton';
 import bcrypt from 'bcryptjs';
