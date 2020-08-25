8dde4337ab78b3c12aa8922025474703aab8a0a5
Ton Binh On <nick.on437@gmail.com>
2020-08-26 00:57:10 +1000

    Componentize the Sidebar; Add pointer cursor

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { StartButton, StopButton } from './components/elements/Button';
 import { InputArea, OutputArea } from './components/elements/IOArea';
 import { NetPeripheral, USBPeripheral } from './components/elements/Peripherals';
 import Switch from './components/elements/Switch';
+import Sidebar from './components/elements/Sidebar';
 
 const App = () => (
   <DisplayProvider>
@@ -29,16 +30,7 @@ const App = () => (
         <InputArea />
       </div>
     </div>
-    <ul className="sidebar">
-      <li className="sidebar-item">
-        <span className="sidebar-item-text">Distance learning</span>
-        <svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="youtube" className="svg-inline--fa fa-youtube fa-w-18" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="currentColor" d="M549.655 124.083c-6.281-23.65-24.787-42.276-48.284-48.597C458.781 64 288 64 288 64S117.22 64 74.629 75.486c-23.497 6.322-42.003 24.947-48.284 48.597-11.412 42.867-11.412 132.305-11.412 132.305s0 89.438 11.412 132.305c6.281 23.65 24.787 41.5 48.284 47.821C117.22 448 288 448 288 448s170.78 0 213.371-11.486c23.497-6.321 42.003-24.171 48.284-47.821 11.412-42.867 11.412-132.305 11.412-132.305s0-89.438-11.412-132.305zm-317.51 213.508V175.185l142.739 81.205-142.739 81.201z" /></svg>
-      </li>
-      <li className="sidebar-item">
-        <span className="sidebar-item-text">Help</span>
-        <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="file-alt" className="svg-inline--fa fa-file-alt fa-w-12" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path fill="currentColor" d="M224 136V0H24C10.7 0 0 10.7 0 24v464c0 13.3 10.7 24 24 24h336c13.3 0 24-10.7 24-24V160H248c-13.2 0-24-10.8-24-24zm64 236c0 6.6-5.4 12-12 12H108c-6.6 0-12-5.4-12-12v-8c0-6.6 5.4-12 12-12h168c6.6 0 12 5.4 12 12v8zm0-64c0 6.6-5.4 12-12 12H108c-6.6 0-12-5.4-12-12v-8c0-6.6 5.4-12 12-12h168c6.6 0 12 5.4 12 12v8zm0-72v8c0 6.6-5.4 12-12 12H108c-6.6 0-12-5.4-12-12v-8c0-6.6 5.4-12 12-12h168c6.6 0 12 5.4 12 12zm96-114.1v6.1H256V0h6.1c6.4 0 12.5 2.5 17 7l97.9 98c4.5 4.5 7 10.6 7 16.9z"></path></svg>
-      </li>
-    </ul>
+    <Sidebar />
   </DisplayProvider>
 );
 
