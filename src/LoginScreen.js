90b2badd27e3c7b3e9d4b800b69a4a8d630ec5b2
czzlyin <62701549+czzlyin@users.noreply.github.com>
2020-08-26 15:13:06 +1000

    Update LoginScreen.js
    updated with new json file

--- src/LoginScreen.js
+++ src/LoginScreen.js
@@ -3,17 +3,20 @@ import bcrypt from 'bcryptjs';
 import App from './App';
 import './LoginScreen.css';
 
+const data = require('./hash/hash.json');
+
+// read from json
+const hashValue = data.find((hash) => hash.id === 1);
+
 function LoginScreen() {
   // password
   const [password, setPassword] = useState('');
   const setInput = (e) => setPassword(e.target.value);
-  // hash value
-  const hash = '$2b$09$xL3tXFifu0DWY/OYGvRD4eiDE0atzsOIWHtyL99fY/7WXYNSt0Z7a';
-
   // login button
   const [log, setLog] = useState(false);
   const login = () => {
-    const check = bcrypt.compareSync(password, hash);
+    // const hash = fs.readFileSync('hash.txt');
+    const check = bcrypt.compareSync(password, hashValue.value);
     if (check) {
       setLog(true);
     } else {
@@ -29,26 +32,26 @@ function LoginScreen() {
 
   return (
 
-    <div className="app">
-      <div className="container">
-        <div className="loginScreen">
-          Please Login
-          <input
-            type="password"
-            className="input"
-            value={password}
-            onChange={setInput}
-          />
-          <button
-            type="submit"
-            onClick={login}
-            className="btn"
-          >
-            Login
-          </button>
-        </div>
+  // <div className="app">
+    <div className="container">
+      <div className="loginScreen">
+        Please Login
+        <input
+          type="password"
+          className="input"
+          value={password}
+          onChange={setInput}
+        />
+        <button
+          type="submit"
+          onClick={login}
+          className="btn"
+        >
+          Login
+        </button>
       </div>
     </div>
+  // </div>
 
   );
 }
