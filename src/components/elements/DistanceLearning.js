17df5d42162b4ba458f9f0d154504b603100a82a
Ton Binh On <nick.on437@gmail.com>
2020-08-29 20:47:19 +1000

    Fix console warning

--- src/components/elements/DistanceLearning.js
+++ src/components/elements/DistanceLearning.js
@@ -38,6 +38,7 @@ const DistanceLearning = (props) => {
     .map((content) => (
       <DistanceLearningContent
         id={content.id}
+        key={content.id}
         week={content.week}
         level={content.level}
         title={content.title}
@@ -66,9 +67,8 @@ const DistanceLearning = (props) => {
           <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="search" className="svg-inline--fa fa-search fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z" /></svg>
           <input type="text" placeholder="Search for names.." className="control" onKeyUp={handleSearchInput} />
         </div>
-        {/* <div className="hflex"> */}
         <div className="control-container">
-          <label forHtml="levels">Level:</label>
+          <label forhtml="levels">Level:</label>
           <select name="levels" id="levels" className="control" onChange={handleChangeLevel}>
             <option value="all">All</option>
             <option value="toddler">Toddler</option>
@@ -78,7 +78,7 @@ const DistanceLearning = (props) => {
           </select>
         </div>
         <div className="control-container">
-          <label forHtml="weeks">Week:</label>
+          <label forhtml="weeks">Week:</label>
           <select name="weeks" id="weeks" className="control" onChange={handleChangeWeek}>
             <option value="all">All</option>
             <option value="w1">1</option>
@@ -87,7 +87,6 @@ const DistanceLearning = (props) => {
             <option value="w4">4</option>
           </select>
         </div>
-        {/* </div> */}
       </div>
       <div id="dist-learn-content-container">
         {contentList}
