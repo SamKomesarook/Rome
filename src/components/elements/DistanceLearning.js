55142d8c2ff47bee14de5292b3444403dbca5400
Ton Binh On <nick.on437@gmail.com>
2020-08-29 16:48:03 +1000

    Implement functional search bar

--- src/components/elements/DistanceLearning.js
+++ src/components/elements/DistanceLearning.js
@@ -11,6 +11,7 @@ const DistanceLearning = (props) => {
     },
   ];
 
+  const [searchInput, setSearchInput] = useState('');
   const [level, setLevel] = useState('all');
   const [week, setWeek] = useState('all');
 
@@ -31,6 +32,7 @@ const DistanceLearning = (props) => {
   };
 
   const contentList = DATA
+    .filter((content) => content.title.toLowerCase().includes(searchInput.toLowerCase()))
     .filter(LEVEL_FILTER_MAP[level])
     .filter(WEEK_FILTER_MAP[week])
     .map((content) => (
@@ -43,8 +45,6 @@ const DistanceLearning = (props) => {
       />
     ));
 
-  const isActiveClass = props.isActive ? '' : ' hidden';
-
   const handleChangeLevel = (e) => {
     setLevel(e.target.value);
   };
@@ -53,12 +53,18 @@ const DistanceLearning = (props) => {
     setWeek(e.target.value);
   };
 
+  const handleSearchInput = (e) => {
+    setSearchInput(e.target.value);
+  };
+
+  const isActiveClass = props.isActive ? '' : ' hidden';
+
   return (
     <div id="distance-learning" className={`sidebar-item-pane${isActiveClass}`}>
       <div id="filter-control">
         <div className="control-container">
           <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="search" className="svg-inline--fa fa-search fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z" /></svg>
-          <input type="text" placeholder="Search for names.." className="control" />
+          <input type="text" placeholder="Search for names.." className="control" onKeyUp={handleSearchInput} />
         </div>
         {/* <div className="hflex"> */}
         <div className="control-container">
