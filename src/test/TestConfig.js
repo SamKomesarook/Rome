34a7d4943cc80d194d949f7ab955b9d355e52061
Ton Binh On <nick.on437@gmail.com>
2020-09-08 17:19:56 +1000

    Fix - Add missing url

--- src/test/TestConfig.js
+++ src/test/TestConfig.js
@@ -1,5 +1,6 @@
 class TestConfig {
-    static URL = 'http://localhost:3000/';
+    static URL = 'http://localhost:4444/wd/hub'
+    // static URL = 'http://localhost:3000/';
 }
 
 export default TestConfig;
