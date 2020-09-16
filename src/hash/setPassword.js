ed7b8c090a615009f960b88dfff6a39b8307065c
czzlyin <62701549+czzlyin@users.noreply.github.com>
2020-09-16 13:12:29 +1000

    fix the setPassword bug
    fix the setPassword bug

--- src/hash/setPassword.js
+++ src/hash/setPassword.js
@@ -3,16 +3,15 @@ const fs = require('fs');
 
 const pass = process.argv[2];
 
-const hash = bcrypt.hashSync(pass, 9);
+if ((pass === undefined)) {
+  console.log('new password can not be empty');
+} else {
+  const hash = bcrypt.hashSync(pass, 9);
 
-const strHash = `"value":"${hash}"`;
-const id = '"id":1,';
+  const strHash = `"value":"${hash}"`;
+  const id = '"id":1,';
+  const file = `[{${id}${strHash}}]`;
 
-const file = `[{${id}${strHash}}]`;
-
-if(process.argv[3] === "true"){
-    fs.writeFileSync('./src/hash/hash.json', file);
-}else{
-    fs.writeFileSync('hash.json', file);
+  fs.writeFileSync('./src/hash/hash.json', file);
+  console.log('new password has been set');
 }
-console.log('new password has been set');
