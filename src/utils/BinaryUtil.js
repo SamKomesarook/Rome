ddd3635d06248515edcfe4a3b67038b31555ac2d
Ton Binh On <nick.on437@gmail.com>
2020-09-20 18:02:50 +1000

    New - Allows negative binary form for int

--- src/utils/BinaryUtil.js
+++ src/utils/BinaryUtil.js
@@ -11,29 +11,36 @@ class BinaryUtil {
         return '0'.repeat(pad) + binary;
       }).join('');
 
-      const remainingBitsLength = bit - binary.length;
-      let remainingBits = '';
-      for (let i = 0; i < remainingBitsLength; i++) {
-        remainingBits = remainingBits.concat('0');
-      }
-      return remainingBits.concat(binary);
+      return this.getFilling(bit - binary.length, '0') + binary;
     }
 
     static num2Bin = (num, byte = 2) => {
       const bit = byte * 8;
-      const binary = (num >>> 0).toString(2);
+
+      // Positize number to maintain the length of binary to only the significant part
+      let positiveValue;
+      if (num >= 0) {
+        positiveValue = num;
+      } else {
+        positiveValue = num * -1;
+      }
+
+      // Convert num to binary
+      const binary = (positiveValue >>> 0).toString(2);
 
       const remainingBitsLength = bit - binary.length;
-      let remainingBits = '';
-      for (let i = 0; i < remainingBitsLength; i++) {
-        remainingBits = remainingBits.concat('0');
+
+      if (num >= 0) {
+        return this.getFilling(remainingBitsLength, '0') + binary;
+      } else {
+        const invertedBinary = binary.replace(/1/g, '2').replace(/0/g, '1').replace(/2/g, '0');
+        return this.getFilling(remainingBitsLength, '1') + invertedBinary;
       }
-      return remainingBits.concat(binary);
     };
 
     static dec2Bin = (dec, byte = 4) => {
       const exponentMemoryAllocation = 8;
-      const significantMemoryAllocaiton = byte * 8 - exponentMemoryAllocation - 1;
+      const significantMemoryAllocation = byte * 8 - exponentMemoryAllocation - 1;
 
       // Convert the input into binary
       const binary = dec.toString(2);
@@ -52,11 +59,15 @@ class BinaryUtil {
 
       const decBinary = sign + biasExponentBinary + significant;
 
-      let remainingBits = '';
-      for (let i = 0; i < significantMemoryAllocaiton; i++) {
-        remainingBits = remainingBits.concat('0');
+      return decBinary + this.getFilling(significantMemoryAllocation - significant.length, '0');
+    }
+
+    static getFilling = (remainingLength, character) => {
+      let filling = '';
+      for (let i = 0; i < remainingLength; i++) {
+        filling = filling.concat(character);
       }
-      return decBinary + remainingBits;
+      return filling;
     }
 }
 
