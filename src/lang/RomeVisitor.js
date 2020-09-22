a317dbb0516b65c730d2439a75b8ba2983e309f1
czzlyin <62701549+czzlyin@users.noreply.github.com>
2020-09-22 18:08:23 +1000

    add in float and negative number (combine with refactor)
    1. add in float to represent decimal numbers
    2. add in negative numbers
    3. already combine with the refactor class that Nick provide

--- src/lang/RomeVisitor.js
+++ src/lang/RomeVisitor.js
@@ -4,10 +4,9 @@ import { NumContext } from './grammar/Rome/RomeParser';
 
 // TODO some updates use setDisplay. Should we?
 class RVisitor extends RomeVisitor {
-  constructor(display, setDisplay, errorReporter) {
+  constructor(staticDisplay, errorReporter) {
     super();
-    this.display = display;
-    this.setDisplay = setDisplay;
+    this.staticDisplay = staticDisplay;
     this.errorReporter = errorReporter;
   }
 
@@ -29,13 +28,14 @@ class RVisitor extends RomeVisitor {
     return this.visitChildren(ctx);
   }
 
+  visitEnd(ctx) {
+
+  }
+
   visitSet(ctx) {
-    const newMemory = this.display.memory;
-    newMemory[this.display.selected].type = this.visitChildren(ctx)[2]; // TODO no need to visit all of the children, just need the args
-    this.setDisplay((display) => ({
-      ...display,
-      memory: newMemory,
-    }));
+    const { selected } = this.staticDisplay;
+    const newType = this.visitChildren(ctx)[2]; // TODO no need to visit all of the children, just need the args
+    this.staticDisplay.memory[selected].type = newType;
   }
 
   visitLoop(ctx) {
@@ -49,16 +49,16 @@ class RVisitor extends RomeVisitor {
       return;
     }
     for (let i = 0; i < upperBound; i++) {
-      this.display.commands.unshift(ctx.expressions());
-      this.display.commands = this.display.commands.flat(Infinity); // TODO is the assignment really necessary?
+      this.staticDisplay.commands.unshift(ctx.expressions());
+      this.staticDisplay.commands = this.staticDisplay.commands.flat(Infinity); // TODO is the assignment really necessary?
     }
   }
 
   visitMem(ctx) {
     if (ctx.strargs() != null) {
       const arg = this.visitChildren(ctx.strargs())[0];
-      for (let i = 0; i < this.display.memory.length; i++) {
-        const mem = this.display.memory[i];
+      for (let i = 0; i < this.staticDisplay.memory.length; i++) {
+        const mem = this.staticDisplay.memory[i];
         if (mem.name === arg) {
           return mem.content;
         }
@@ -66,7 +66,7 @@ class RVisitor extends RomeVisitor {
       this.errorReporter.generalError('No memory with that name');
     } else if (ctx.intargs().constructor === NumContext) {
       try {
-        return this.display.memory[parseInt(this.visitChildren(ctx.intargs())) - 1].content;
+        return this.staticDisplay.memory[parseInt(this.visitChildren(ctx.intargs())) - 1].content;
       } catch (e) {
         this.errorReporter.generalError('Cannot parse memory argument');
         return null;
@@ -78,28 +78,43 @@ class RVisitor extends RomeVisitor {
 
   visitMove(ctx) {
     if (this.visitChildren(ctx)[2] === 'next') {
-      const maxUsableMemoryKey = this.display.memorySize - this.display.specialKeys.length - 1;
-      if (this.display.selected === maxUsableMemoryKey) {
+      const maxUsableMemoryKey = this.staticDisplay.memorySize - this.staticDisplay.specialKeys.length - 1;
+      if (this.staticDisplay.selected === maxUsableMemoryKey) {
         this.errorReporter.generalError('No more memory');
         return;
       }
-      this.display.selected += 1;
+      this.staticDisplay.selected += 1;
     } else {
-      if (this.display.selected === 0) {
+      if (this.staticDisplay.selected === 0) {
         this.errorReporter.generalError('No more memory');
         return;
       }
-      this.display.selected -= 1;
+      this.staticDisplay.selected -= 1;
     }
+  }
+
+  // search for decimal point:
+  checkDec(ctx) {
+    let arg = this.visitChildren(ctx)[2];
+    let dec = 0;
+    if (typeof arg === 'object') {
+      arg = arg[0];
+    }
+    for (let i = 0; i < arg.length + 1; i++) {
+      if (arg[i] === '.') {
+        dec += 1;
+      }
+    }
+    return dec;
   }
 
   visitWrite(ctx) {
     // TODO check for maximum length (or spillover to the next memory cell?
-    if (this.display.memory[this.display.selected].content !== '') {
+    if (this.staticDisplay.memory[this.staticDisplay.selected].content !== '') {
       this.errorReporter.generalError('Memory cell not empty');
       return;
     }
-    if (this.display.memory[this.display.selected].type === '') {
+    if (this.staticDisplay.memory[this.staticDisplay.selected].type === '') {
       this.errorReporter.generalError('Memory type not set');
       return;
     }
@@ -107,93 +122,125 @@ class RVisitor extends RomeVisitor {
     if (typeof arg === 'object') {
       arg = arg[0];
     }
-    // range of int:
-    if (arg[0] === '"' && this.display.memory[this.display.selected].type === 'integer') {
-      this.errorReporter.generalError('Wrong memory type for writing'); // write letters in numbers type
+
+    // int:
+    if (isNaN(arg) && this.staticDisplay.memory[this.staticDisplay.selected].type === 'integer') {
+      this.errorReporter.generalError('Wrong memory type for writing');
       return;
     }
-    if (arg[0] !== '"' && this.display.memory[this.display.selected].type === 'integer') {
+    if (!isNaN(arg) && this.staticDisplay.memory[this.staticDisplay.selected].type === 'integer') {
       const inNum = Number(arg);
-      if (inNum > 65535) {
+      if (inNum > 65535 || inNum < -65535) {
         this.errorReporter.generalError('out of maximun memory');
+        return;
+      }
+      if (this.checkDec(ctx) > 0) {
+        this.errorReporter.generalError('Wrong memory type of writing');
         return;
       }
     }
 
-    // range of long:
-    if (arg[0] === '"' && this.display.memory[this.display.selected].type === 'long') {
-      this.errorReporter.generalError('Wrong memory type for writing'); // write letters in numbers type
+    // long:
+    if (isNaN(arg) && this.staticDisplay.memory[this.staticDisplay.selected].type === 'long') {
+      this.errorReporter.generalError('Wrong memory type for writing');
       return;
     }
-    if (arg[0] !== '"' && this.display.memory[this.display.selected].type === 'long') {
+    if (!isNaN(arg) && this.staticDisplay.memory[this.staticDisplay.selected].type === 'long') {
       const inNum = Number(arg);
-      if (inNum > 4294967295) {
+      if (inNum > 4294967295 || inNum < -4294967295) {
         this.errorReporter.generalError('out of maximun memory');
         return;
+      }
+      if (this.checkDec(ctx) > 0) {
+        this.errorReporter.generalError('Wrong memory type of writing');
+        return;
+      }
+    }
+
+    // float:
+    if (isNaN(arg) && this.staticDisplay.memory[this.staticDisplay.selected].type === 'float') {
+      this.errorReporter.generalError('Wrong memory type for writing');
+      return;
+    }
+    if (!isNaN(arg) && this.staticDisplay.memory[this.staticDisplay.selected].type === 'float') {
+      const inNum = Number(arg);
+      if (inNum > 4294967295 || inNum < -4294967295) {
+        this.errorReporter.generalError('out of maximun memory');
+        return;
+      }
+      if (this.checkDec(ctx) === 0) {
+        arg += '.00';
+      }
+      if (this.checkDec(ctx) > 1) {
+        this.errorReporter.generalError('Wrong memory type of writing');
+        return;
+      }
+      if (this.checkDec(ctx) === 1) {
+        if (arg.split('.')[1].length > 16) {
+          this.errorReporter.generalError('out of maximun memory');
+          return;
+        }
       }
     }
 
     // char:
-    if (arg[0] !== '"' && this.display.memory[this.display.selected].type === 'character') {
-      this.errorReporter.generalError('Wrong memory type for writing'); // write numbers in letters type
+    if (arg[0] !== '"' && this.staticDisplay.memory[this.staticDisplay.selected].type === 'character') {
+      this.errorReporter.generalError('Wrong memory type for writing');
       return;
     }
-    if (arg[0] === '"' && this.display.memory[this.display.selected].type === 'character') {
+    if (arg[0] === '"' && this.staticDisplay.memory[this.staticDisplay.selected].type === 'character') {
       if (arg.length > 3) {
         this.errorReporter.generalError('out of maximun memory');
         return;
       }
     }
 
-    // string: upper-bond 6 bytes per cell, up to 12*6 72 bytes
-    if (arg[0] !== '"' && this.display.memory[this.display.selected].type === 'string') {
+    // string:
+    if (arg[0] !== '"' && this.staticDisplay.memory[this.staticDisplay.selected].type === 'string') {
       this.errorReporter.generalError('Wrong memory type for writing'); // write numbers in letters type
       return;
     }
-
-    if (arg[0] === '"' && this.display.memory[this.display.selected].type === 'string') {
+    if (arg[0] === '"' && this.staticDisplay.memory[this.staticDisplay.selected].type === 'string') {
       if (arg.length > 72) {
         this.errorReporter.generalError('out of maximun memory');
         return;
       }
-      if (arg.length > (72 - this.display.memory[this.display.selected].key * 6)) {
+      if (arg.length > (72 - this.staticDisplay.memory[this.staticDisplay.selected].key * 6)) {
         this.errorReporter.generalError('out of maximun memory');
         return;
       }
     }
 
     // Get the keys of special memory cells
-    const usbMemoryKey = this.display.specialKeys.find((element) => element.specialContent === 'usb').key;
-    if (this.display.selected === usbMemoryKey) {
+    const usbMemoryKey = this.staticDisplay.specialKeys.find((element) => element.specialContent === 'usb').key;
+    if (this.staticDisplay.selected === usbMemoryKey) {
       USBToggle();
     } else {
-      if (arg[0] !== '"') {
-        this.display.memory[this.display.selected].content = arg;
-      }
+      this.staticDisplay.memory[this.staticDisplay.selected].content = arg;
       if (arg[0] === '"') {
         arg = arg.substr(0, arg.length - 1);
         arg = arg.substr(1, arg.length - 1);
 
-        if (this.display.memory[this.display.selected].type === 'string') {
-          const pos = this.display.memory[this.display.selected].key;
+        if (this.staticDisplay.memory[this.staticDisplay.selected].type === 'string') {
+          const pos = this.staticDisplay.memory[this.staticDisplay.selected].key;
           const base = Math.floor(arg.length / 6);
           for (let i = 0; i < base + 1; i++) {
-            this.display.memory[pos + i * 1].content = arg.substr(i * 6, 6);
+            this.staticDisplay.memory[pos + i * 1].content = arg.substr(i * 6, 6);
           }
           return;
         }
-        this.display.memory[this.display.selected].content = arg;
+        this.staticDisplay.memory[this.staticDisplay.selected].content = arg;
       }
     }
   }
 
   visitFree(ctx) {
-    this.display.memory[this.display.selected].content = '';
+    this.staticDisplay.memory[this.staticDisplay.selected].content = '';
   }
 
   visitIf(ctx) {
     const condInput = this.visitChildren(ctx.conditional());
-    const memoryCell = this.display.memory[this.display.selected];
+    const memoryCell = this.staticDisplay.memory[this.staticDisplay.selected];
 
     let leftValue;
     const rightValue = condInput[4][0];
@@ -216,22 +263,22 @@ class RVisitor extends RomeVisitor {
       || (compareKeyword === 'greater' && leftValue > rightValue)
       // eslint-disable-next-line eqeqeq
       || (compareKeyword === 'equal' && leftValue == rightValue)) {
-        this.display.commands.unshift(ctx.expressions());
-        this.display.commands = this.display.commands.flat(Infinity);
+        this.staticDisplay.commands.unshift(ctx.expressions());
+        this.staticDisplay.commands = this.staticDisplay.commands.flat(Infinity);
       }
     } else if (condInput[0] === 'not') {
       if ((compareKeyword === 'less' && leftValue >= rightValue)
       || (compareKeyword === 'greater' && leftValue <= rightValue)
       // eslint-disable-next-line eqeqeq
       || (compareKeyword === 'equal' && leftValue != rightValue)) {
-        this.display.commands.unshift(ctx.expressions());
-        this.display.commands = this.display.commands.flat(Infinity);
+        this.staticDisplay.commands.unshift(ctx.expressions());
+        this.staticDisplay.commands = this.staticDisplay.commands.flat(Infinity);
       }
     }
   }
 
   visitKread(ctx) {
-    if (!this.display.importIO) {
+    if (!this.staticDisplay.importIO) {
       this.errorReporter.generalError("Unknown function 'k_read'");
     }
     // TODO is this necessary?
@@ -240,7 +287,7 @@ class RVisitor extends RomeVisitor {
   }
 
   visitSwrite(ctx) {
-    if (!this.display.importIO) {
+    if (!this.staticDisplay.importIO) {
       this.errorReporter.generalError("Unknown function 's_write'");
       return;
     }
@@ -249,11 +296,11 @@ class RVisitor extends RomeVisitor {
       arg = arg[0];
     }
     // TODO if string, print with parenthesis?
-    this.display.output = this.display.output.concat(arg.replace('"', '').replace('"', ''), '\n');
+    this.staticDisplay.output = this.staticDisplay.output.concat(arg.replace('"', '').replace('"', ''), '\n');
   }
 
   visitIo(ctx) {
-    this.display.importIO = true;
+    this.staticDisplay.importIO = true;
   }
 
   visitName(ctx) {
@@ -265,94 +312,45 @@ class RVisitor extends RomeVisitor {
       this.errorReporter.generalError('Cannot name a memory area as a number');
       return;
     }
-    this.display.memory[this.display.selected].name = arg;
+    this.staticDisplay.memory[this.staticDisplay.selected].name = arg;
   }
 
   visitPaint(ctx) {
     const newValue = this.visitChildren(ctx)[2];
-    // Update display.outputStyle.bgColor
-    this.setDisplay((prevDisplay) => ({
-      ...prevDisplay,
-      outputStyle: {
-        ...prevDisplay.outputStyle,
-        bgColor: newValue,
-      },
-    }));
+    this.staticDisplay.outputStyle.bgColor = newValue;
   }
 
   visitTextColor(ctx) {
     const newValue = this.visitChildren(ctx)[2];
-    // Update display.outputStyle.bgColor
-    this.setDisplay((prevDisplay) => ({
-      ...prevDisplay,
-      outputStyle: {
-        ...prevDisplay.outputStyle,
-        txtColor: newValue,
-      },
-    }));
+    this.staticDisplay.outputStyle.txtColor = newValue;
   }
 
   visitTextSize(ctx) {
     const newValue = this.visitChildren(ctx)[2];
-    // Update display.outputStyle.bgColor
-    this.setDisplay((prevDisplay) => ({
-      ...prevDisplay,
-      outputStyle: {
-        ...prevDisplay.outputStyle,
-        txtSize: newValue,
-      },
-    }));
+    this.staticDisplay.outputStyle.txtSize = newValue;
   }
 
   visitTextAlign(ctx) {
     const newValue = this.visitChildren(ctx)[2];
-    // Update display.outputStyle.bgColor
-    this.setDisplay((prevDisplay) => ({
-      ...prevDisplay,
-      outputStyle: {
-        ...prevDisplay.outputStyle,
-        txtAlign: newValue,
-      },
-    }));
+    this.staticDisplay.outputStyle.txtAlign = newValue;
   }
 
   visitBold(ctx) {
     const isBold = (this.visitChildren(ctx)[2] === 'true');
     const newValue = isBold ? 'bold' : '';
-    // Update display.outputStyle.bold
-    this.setDisplay((prevDisplay) => ({
-      ...prevDisplay,
-      outputStyle: {
-        ...prevDisplay.outputStyle,
-        bold: newValue,
-      },
-    }));
+    this.staticDisplay.outputStyle.bold = newValue;
   }
 
   visitItalic(ctx) {
     const isItalic = (this.visitChildren(ctx)[2] === 'true');
     const newValue = isItalic ? 'italic' : '';
-    // Update display.outputStyle.italic
-    this.setDisplay((prevDisplay) => ({
-      ...prevDisplay,
-      outputStyle: {
-        ...prevDisplay.outputStyle,
-        italic: newValue,
-      },
-    }));
+    this.staticDisplay.outputStyle.italic = newValue;
   }
 
   visitUnderline(ctx) {
     const isUnderline = (this.visitChildren(ctx)[2] === 'true');
     const newValue = isUnderline ? 'underline' : '';
-    // Update display.outputStyle.underline
-    this.setDisplay((prevDisplay) => ({
-      ...prevDisplay,
-      outputStyle: {
-        ...prevDisplay.outputStyle,
-        underline: newValue,
-      },
-    }));
+    this.staticDisplay.outputStyle.underline = newValue;
   }
 }
 
