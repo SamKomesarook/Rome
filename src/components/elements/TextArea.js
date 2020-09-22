e29633176c67fcb210fbe213dbe395379e8c1d53
Ton Binh On <nick.on437@gmail.com>
2020-09-22 23:44:33 +1000

    Improve - Keep cursor in place when interact w/ char at the end and start of a line

--- src/components/elements/TextArea.js
+++ src/components/elements/TextArea.js
@@ -48,7 +48,7 @@ const TextArea = () => {
     return textSegments;
   };
 
-  function restoreSelection(absoluteAnchorIndex, absoluteFocusIndex) {
+  function restoreSelection(absolateAnchorNode, absoluteAnchorIndex, absoluteFocusNode, absoluteFocusIndex, absoluteSelSegmentIndex) {
     const sel = window.getSelection();
     const textSegments = getTextSegments(textArea.current);
     let anchorNode = textArea.current;
@@ -56,21 +56,60 @@ const TextArea = () => {
     let focusNode = textArea.current;
     let focusIndex = 0;
     let currentIndex = 0;
-    textSegments.forEach(({ text, node }) => {
-      const startIndexOfNode = currentIndex;
-      const endIndexOfNode = startIndexOfNode + text.length;
-      if (startIndexOfNode <= absoluteAnchorIndex && absoluteAnchorIndex <= endIndexOfNode) {
-        anchorNode = node;
-        anchorIndex = absoluteAnchorIndex - startIndexOfNode;
-      }
-      if (startIndexOfNode <= absoluteFocusIndex && absoluteFocusIndex <= endIndexOfNode) {
-        focusNode = node;
-        focusIndex = absoluteFocusIndex - startIndexOfNode;
-      }
-      currentIndex += text.length;
-    });
 
+    let hasAnchorIndex = false;
+    let hasFocusIndex = false;
+    let currentSegmentIndex = 0;
+    // textSegments.forEach(({ text, node }) => {
+    //   console.log('text in segment', text);
+    //   console.log('node in segment', node);
+    //   const startIndexOfNode = currentIndex;
+    //   const endIndexOfNode = startIndexOfNode + text.length;
+    //   if (startIndexOfNode <= absoluteAnchorIndex && absoluteAnchorIndex <= endIndexOfNode && currentSegmentIndex === absoluteSelSegmentIndex) {
+    //     console.log('absoluteAnchorIndex is in range');
+    //     anchorNode = node;
+    //     anchorIndex = absoluteAnchorIndex - startIndexOfNode;
+    //     // hasAnchorIndex = true;
+    //   }
+    //   if (startIndexOfNode <= absoluteFocusIndex && absoluteFocusIndex <= endIndexOfNode && currentSegmentIndex === absoluteSelSegmentIndex) {
+    //     console.log('absoluteFocusIndex is in range');
+    //     focusNode = node;
+    //     focusIndex = absoluteFocusIndex - startIndexOfNode;
+    //     // hasFocusIndex = true;
+    //   }
+    //   currentIndex += text.length;
+    // });
+    
+    for (let i = 0; i < textSegments.length; i++) {
+      console.log('text in segment', textSegments[i].text);
+      console.log('node in segment', textSegments[i].node);
+      const startIndexOfNode = currentIndex;
+      const endIndexOfNode = startIndexOfNode + textSegments[i].text.length;
+      if (startIndexOfNode <= absoluteAnchorIndex && absoluteAnchorIndex <= endIndexOfNode && i === absoluteSelSegmentIndex) {
+        console.log('absoluteAnchorIndex is in range');
+        anchorNode = textSegments[i].node;
+        anchorIndex = absoluteAnchorIndex - startIndexOfNode;
+        // hasAnchorIndex = true;
+      }
+      if (startIndexOfNode <= absoluteFocusIndex && absoluteFocusIndex <= endIndexOfNode && i === absoluteSelSegmentIndex) {
+        console.log('absoluteFocusIndex is in range');
+        focusNode = textSegments[i].node;
+        focusIndex = absoluteFocusIndex - startIndexOfNode;
+        // hasFocusIndex = true;
+      }
+      currentIndex += textSegments[i].text.length;
+    }
+
+    console.log('To focus: anchorNode', anchorNode);
+    console.log('To focus: abosulateAnchorNode', absolateAnchorNode);
+    console.log('To focus: anchorIndex', anchorIndex);
+    console.log('To focus: focusNode', focusNode);
+    console.log('To focus: focusIndex', focusIndex);
+    console.log('To focus: absoluteFocusNode', absoluteFocusNode);
+
+    console.log(`anchorNode and absoluteAnchorNode is the same ${anchorNode === absolateAnchorNode}`);
     sel.setBaseAndExtent(anchorNode, anchorIndex, focusNode, focusIndex);
+    // sel.setBaseAndExtent(absolateAnchorNode, anchorIndex, absoluteFocusNode, focusIndex);
   }
 
   function renderText(text) {
@@ -89,19 +128,12 @@ const TextArea = () => {
   }
 
   const updateColor = () => {
-    const lines = textArea.current.innerHTML
-      .replace(/<span...>/g, '')
-      .replace(/<\/span>/g, '')
-      .replace(/<\/div>/g, '')
-      .split('<div>');
+    const lines = textArea.current.innerText.split('\n');
 
-    // Remove 'weird' empty \n added at the start
-    if (lines.length > 1) {
-      lines.shift();
-    }
-
+    console.log('lines', lines);
     const output = lines.map((line) => {
       const words = line.split(' ');
+      console.log('words', words);
       const lineOutput = words.map((word) => {
         if (word === 'start') {
           // return `<div style='color: ${colorCode.macroWrapper}'>${word}</div>`;
@@ -116,39 +148,138 @@ const TextArea = () => {
   };
 
   function updateEditor() {
+    console.log('window', window);
     const sel = window.getSelection();
+    console.log('sel', sel);
+
     const textSegments = getTextSegments(textArea.current);
+    console.log('textSegments', textSegments);
+
     const textContent = textSegments.map(({ text }) => text).join('');
+    console.log('textContent', textContent);
+
     let anchorIndex = null;
     let focusIndex = null;
     let currentIndex = 0;
-    textSegments.forEach(({ text, node }) => {
-      if (node === sel.anchorNode) {
+
+    let selSegmentIndex = 0;
+
+    const anchorNode = sel.anchorNode;
+    const focusNode = sel.focusNode;
+    // anchorIndex = sel.anchorOffset;
+    console.log('anchorNode', anchorNode);
+    console.log('focusNode', focusNode);
+
+    // textSegments.forEach(({ text, node }) => {
+    //   if (node === sel.anchorNode) {
+    //     anchorIndex = currentIndex + sel.anchorOffset;
+    //     // break;
+    //   }
+    //   if (node === sel.focusNode) {
+    //     focusIndex = currentIndex + sel.focusOffset;
+    //   }
+    //   currentIndex += text.length;
+
+    //   selSegmentIndex++;
+    // });
+    for (let i = 0; i < textSegments.length; i++) {
+      if (textSegments[i].node === sel.anchorNode) {
         anchorIndex = currentIndex + sel.anchorOffset;
+        selSegmentIndex = i;
       }
-      if (node === sel.focusNode) {
+      if (textSegments[i].node === sel.focusNode) {
         focusIndex = currentIndex + sel.focusOffset;
       }
-      currentIndex += text.length;
-    });
+      currentIndex += textSegments[i].text.length;
+    }
+
+    console.log('anchorIndex', anchorIndex);
+    console.log('focusIndex', focusIndex);
 
     textArea.current.innerHTML = updateColor(textContent);
 
-    restoreSelection(anchorIndex, focusIndex);
+    
+    // console.log('after anchorNode', anchorNode);
+    // console.log('after anchorIndex', anchorIndex);
+    // sel.setPosition(anchorNode, anchorIndex);
+    // sel.setBaseAndExtent()
+    // sel.setBaseAndExtent(anchorNode, sel.anchorOffset, focusNode, sel.focusOffset);
+    restoreSelection(anchorNode, anchorIndex, focusNode, focusIndex, selSegmentIndex);
   }
 
   const diff = (newText, oldText) => {
-    const getDiff = (long, short) => long.split(short).join('');
-    return newText.length > oldText.length ? getDiff(newText, oldText) : getDiff(oldText, newText);
+    // const getDiff = (long, short) => long.split(short).join('');
+    // const getDiff = (long, short) => long.replace(short, '');
+    // const getDiff = (long, short) => long.split(short);
+
+    let isCharactersChanged = false;
+
+    // if (newText.length > oldText.length) {
+    //   newText.replace(oldText, '');
+    // } else {
+    // }
+    const oldTextLines = oldText.split(/\n/);
+    const newTextLines = newText.split(/\n/);
+
+    if (newTextLines.length === oldTextLines.length) {
+      isCharactersChanged = true;
+    } else if (newTextLines.length > oldTextLines.length) {
+      while (true) {
+        let currentOldLine;
+        do {
+          currentOldLine = oldTextLines.shift();
+        } while (currentOldLine !== undefined || currentOldLine === '');
+
+        let currentNewLine;
+        do {
+          currentNewLine = newTextLines.shift();
+        } while (currentNewLine !== undefined || currentNewLine === '');
+
+        if (currentOldLine !== currentNewLine) {
+          isCharactersChanged = true;
+          break;
+        } else if (currentNewLine === undefined || currentOldLine === undefined) {
+          break;
+        }
+      }
+    } else {
+      // let isLeftToRight = true;
+      // while (true) {
+      //   let currentOldLine;
+      //   do {
+      //     currentOldLine = oldTextLines.shift();
+      //   } while (currentOldLine !== undefined || currentOldLine === '');
+
+      //   let currentNewLine;
+      //   do {
+      //     currentNewLine = newTextLines.shift();
+      //   } while (currentNewLine !== undefined || currentNewLine === '');
+
+      //   if (currentOldLine !== currentNewLine) {
+      //     isCharactersChanged = true;
+      //     break;
+      //   } else if (currentNewLine === undefined || currentOldLine === undefined) {
+      //     break;
+      //   }
+      // }
+    }
+
+    return isCharactersChanged;
+    // return newText.length > oldText.length ? getDiff(newText, oldText) : getDiff(oldText, newText);
   };
 
   const handleChange = (event) => {
     event.preventDefault();
-    const txtDiff = diff(textArea.current.innerText, prevInput.current);
-    if (txtDiff.replace(/\n/g, '').length > 0) {
+    // const txtDiff = diff(textArea.current.innerText, prevInput.current);
+    // if (txtDiff.replace(/\n/g, '').length > 0) {
+    //   updateEditor();
+    //   // updateColor();
+    // }
+
+    if (diff(textArea.current.innerText, prevInput.current)) {
       updateEditor();
-      // updateColor();
     }
+
     const value = event.target.innerText;
     // TODO ensure the below includes newline breaks and shit...
     setDisplay((prevDisplay) => ({
