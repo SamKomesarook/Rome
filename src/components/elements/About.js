2c038ee1d0ebe6d5c4a39204838152e1460a4841
sherryysj <45263452+sherryysj@users.noreply.github.com>
2020-09-23 13:08:42 +1000

    set about tab UI, add icon

---
+++ src/components/elements/About.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+const About = ({ isActive }) => {
+
+  const content = "Rome-Education + Julia Brown Schools + (heart sign) = (rome logo)(will update this later) \n\nSpecial thanks to: \nNick On (programming) \nJiecong Zhang (programming) \nShujie Yang (programming) \nErina Ho (gorgeous logo)";
+
+  const isActiveClass = isActive ? '' : ' hidden';
+
+  return (
+    <div id="help" className={`sidebar-item-pane${isActiveClass}`}>
+      <div id="help-content">
+        {content}
+      </div>
+    </div>
+  );
+};
+
+About.propTypes = {
+  isActive: PropTypes.bool.isRequired,
+};
+
+export default About;
