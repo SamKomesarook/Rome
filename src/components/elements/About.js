48a073dd24f9578738d8b513c7754ac0f4917f36
sherryysj <45263452+sherryysj@users.noreply.github.com>
2020-09-23 22:28:10 +1000

    expand the about tab

--- src/components/elements/About.js
+++ src/components/elements/About.js
@@ -3,12 +3,12 @@ import PropTypes from 'prop-types';
 
 const About = ({ isActive }) => {
 
-  const content = "Rome-Education + Julia Brown Schools + (heart sign) = (rome logo)(will update this later) \n\nSpecial thanks to: \nNick On (programming) \nJiecong Zhang (programming) \nShujie Yang (programming) \nErina Ho (gorgeous logo)";
+  const content = "Rome-Education + Julia Brown Schools + (heart sign) = (rome logo)(will update this later) \n\nSpecial thanks to:\nTon Binh On - Nick (programming)\nJiecong Zhang (programming)\nShujie Yang (programming)\nErina Ho (gorgeous logo)";
 
   const isActiveClass = isActive ? '' : ' hidden';
 
   return (
-    <div id="help" className={`sidebar-item-pane${isActiveClass}`}>
+    <div id="help" className={`sidebar-about-pane${isActiveClass}`}>
       <div id="help-content">
         {content}
       </div>
