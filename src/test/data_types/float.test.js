9ccb26e7a518915bb73b41af827204911b282053
Ton Binh On <nick.on437@gmail.com>
2020-09-26 17:19:45 +1000

    New - Add tests cases for new data types

---
+++ src/test/data_types/float.test.js
@@ -0,0 +1,123 @@
+import TestConfig from '../TestConfig';
+
+const webdriver = require('selenium-webdriver');
+
+// Test names
+const testFloatMax = 'test write float with value exceeds maximum';
+const testFloatMin = 'test write float with value exceeds minumum';
+const testValidFloat = 'test write valid float within acceptable range';
+const testNonDecimal = 'test write number without decimal';
+const testFloatQuotes = 'test write number in float quotes';
+
+describe('test float', () => {
+  let driver;
+
+  beforeAll(async () => {
+    driver = new webdriver.Builder().withCapabilities(TestConfig.CAPABILITIES).build();
+    await driver.get(TestConfig.URL);
+    await TestConfig.loginPass(driver);
+  }, 30000);
+
+  afterAll(async () => {
+    await driver.quit();
+  }, 40000);
+
+  afterEach(async () => {
+    const codingArea = await TestConfig.getElementById(driver, 'coding-area');
+    await codingArea.clear();
+
+    const stopBtn = await TestConfig.getElementById(driver, 'reset-button');
+    stopBtn.click();
+  }, 30000);
+
+  test(testFloatMin, async () => {
+    const codingArea = await TestConfig.getElementById(driver, 'coding-area');
+    await codingArea.sendKeys('start\nset(float)\nwrite(-340282346638528859811704183484516925441.0)\nend');
+
+    const startBtn = await TestConfig.getElementById(driver, 'start-button');
+    await startBtn.click();
+
+    const outputArea = await TestConfig.getElementById(driver, 'output-area');
+    const outputAreaRes = await outputArea.getText();
+
+    const memoryCell0 = await TestConfig.getElementById(driver, 'memory-0');
+    const memoryCell0Res = await memoryCell0.getText();
+
+    expect(outputAreaRes).toEqual('Out of memory');
+    expect(memoryCell0Res).toEqual('');
+  }, 35000);
+
+  test(testFloatMax, async () => {
+    const codingArea = await TestConfig.getElementById(driver, 'coding-area');
+    await codingArea.sendKeys('start\nset(float)\nwrite(340282346638528859811704183484516925441.0)\nend');
+
+    const startBtn = await TestConfig.getElementById(driver, 'start-button');
+    await startBtn.click();
+
+    const outputArea = await TestConfig.getElementById(driver, 'output-area');
+    const outputAreaRes = await outputArea.getText();
+
+    const memoryCell0 = await TestConfig.getElementById(driver, 'memory-0');
+    const memoryCell0Res = await memoryCell0.getText();
+
+    expect(outputAreaRes).toEqual('Out of memory');
+    expect(memoryCell0Res).toEqual('');
+  }, 35000);
+
+  test(testValidFloat, async () => {
+    const codingArea = await TestConfig.getElementById(driver, 'coding-area');
+    await codingArea.sendKeys('start\nset(float)\nwrite(340282346638528859811704183484516925440.0)\nmove(next)\nset(float)\nwrite(-340282346638528859811704183484516925440.0)\nmove(next)\nset(float)\nwrite(0.0)\nend');
+
+    const startBtn = await TestConfig.getElementById(driver, 'start-button');
+    await startBtn.click();
+
+    const outputArea = await TestConfig.getElementById(driver, 'output-area');
+    const outputAreaRes = await outputArea.getText();
+
+    const memoryCell0 = await TestConfig.getElementById(driver, 'memory-0');
+    const memoryCell0Res = await memoryCell0.getText();
+    const memoryCell1 = await TestConfig.getElementById(driver, 'memory-1');
+    const memoryCell1Res = await memoryCell1.getText();
+    const memoryCell2 = await TestConfig.getElementById(driver, 'memory-2');
+    const memoryCell2Res = await memoryCell2.getText();
+
+    expect(outputAreaRes).toEqual('');
+    expect(memoryCell0Res).toEqual('340282346638528859811704183484516925440.00');
+    expect(memoryCell1Res).toEqual('-340282346638528859811704183484516925440.00');
+    expect(memoryCell2Res).toEqual('0');
+  }, 35000);
+
+  test(testNonDecimal, async () => {
+    const codingArea = await TestConfig.getElementById(driver, 'coding-area');
+    await codingArea.sendKeys('start\nset(float)\nwrite(10)\nend');
+
+    const startBtn = await TestConfig.getElementById(driver, 'start-button');
+    await startBtn.click();
+
+    const outputArea = await TestConfig.getElementById(driver, 'output-area');
+    const outputAreaRes = await outputArea.getText();
+
+    const memoryCell0 = await TestConfig.getElementById(driver, 'memory-0');
+    const memoryCell0Res = await memoryCell0.getText();
+
+    expect(outputAreaRes).toEqual('');
+    expect(memoryCell0Res).toEqual('10.00');
+  }, 35000);
+
+  test(testFloatQuotes, async () => {
+    const codingArea = await TestConfig.getElementById(driver, 'coding-area');
+    await codingArea.sendKeys('start\nset(float)\nwrite("10")\nend');
+
+    const startBtn = await TestConfig.getElementById(driver, 'start-button');
+    await startBtn.click();
+
+    const outputArea = await TestConfig.getElementById(driver, 'output-area');
+    const outputAreaRes = await outputArea.getText();
+
+    const memoryCell0 = await TestConfig.getElementById(driver, 'memory-0');
+    const memoryCell0Res = await memoryCell0.getText();
+
+    expect(outputAreaRes).toEqual('Wrong memory type for writing');
+    expect(memoryCell0Res).toEqual('');
+  }, 35000);
+});
