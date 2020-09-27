23ffa718896bab210ea3747968ba71180a6cc51b
sherryysj <45263452+sherryysj@users.noreply.github.com>
2020-09-28 08:48:58 +1000

    update logo and title, reset font and layout, white color

--- src/components/elements/About.js
+++ src/components/elements/About.js
@@ -1,16 +1,32 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import Rome from '../../icons/Rome.svg';
 
 const About = ({ isActive }) => {
 
-  const content = "Rome-Education + Julia Brown Schools + (heart sign) = (rome logo)(will update this later) \n\nSpecial thanks to:\nTon Binh On - Nick (programming)\nJiecong Zhang (programming)\nShujie Yang (programming)\nErina Ho (gorgeous logo)";
+  const content = "\nTon Binh On - Nick (programming)\nJiecong Zhang (programming)\nShujie Yang (programming)\nErina Ho (gorgeous logo)";
 
   const isActiveClass = isActive ? '' : ' hidden';
 
   return (
-    <div id="help" className={`sidebar-about-pane${isActiveClass}`}>
-      <div id="help-content">
+    <div id="about" className={`sidebar-about-pane${isActiveClass}`}>
+      <div id="about-content">
+        <span id="about-title">Rome-Education&nbsp;Julia Brown Schools&nbsp;&nbsp;</span> 
+		<svg id="about-heart" width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-heart-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+        <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
+        </svg>
+		<span>&nbsp;&nbsp;</span>
+		<img id="about-rome" src={Rome} width="125" height="125"/>
+		<div id="about-thankTitle">
+		<br></br>
+		<br></br>
+		<br></br>
+		<br></br>
+		Special thanks to:
+		</div>
+		<div id="about-thank">
         {content}
+		</div>
       </div>
     </div>
   );
