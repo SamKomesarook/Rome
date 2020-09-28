bf02f39786452ff10ab75444db0ab88e1917925b
Ton Binh On <nick.on437@gmail.com>
2020-09-29 00:01:51 +1000

    Test - Unlock app-run-view test

--- src/test/sidebar/appRunView.test.js
+++ src/test/sidebar/appRunView.test.js
@@ -2,10 +2,8 @@ import TestConfig from '../TestConfig';
 
 const webdriver = require('selenium-webdriver');
 
-const testName = {
-  testOpenAppRunView: 'test open app run view',
-  testCloseAppRunView: 'test close app run view',
-};
+const testOpenAppRunView = 'test open app run view';
+const testCloseAppRunView = 'test close app run view';
 
 describe('test app run view', () => {
   let driver;
@@ -24,32 +22,32 @@ describe('test app run view', () => {
     await driver.navigate().refresh();
   }, 30000);
 
-  test(testName.testOpenAppRunView, async () => {
-    const appRunViewBtn = await TestConfig.getElementById(driver, 'app-run-view');
+  test(testOpenAppRunView, async () => {
+    const appRunViewBtn = await TestConfig.getElementByName(driver, 'App Run View');
 
     const codingArea = await TestConfig.getElementById(driver, 'coding-area');
-    const switchBtn = await TestConfig.getElementById(driver, 'switch');
+    const switchBtn = await TestConfig.getElementById(driver, 'switch-container');
     const startBtn = await TestConfig.getElementById(driver, 'start-button');
-    const stopBtn = await TestConfig.getElementById(driver, 'stop-button');
-    const memorySection = await TestConfig.getElementById(driver, 'memory-section');
+    const resetBtn = await TestConfig.getElementById(driver, 'reset-button');
+    const memorySection = await TestConfig.getElementByXpath(driver, '//*[@id="computer-column"]/div[1]');
 
     await appRunViewBtn.click();
 
     expect(await codingArea.isDisplayed()).toEqual(false);
     expect(await switchBtn.isDisplayed()).toEqual(false);
     expect(await startBtn.isDisplayed()).toEqual(false);
-    expect(await stopBtn.isDisplayed()).toEqual(false);
+    expect(await resetBtn.isDisplayed()).toEqual(false);
     expect(await memorySection.isDisplayed()).toEqual(false);
   }, 35000);
 
-  test(testName.testOpenAppRunView, async () => {
-    const appRunViewBtn = await TestConfig.getElementById(driver, 'app-run-view');
+  test(testCloseAppRunView, async () => {
+    const appRunViewBtn = await TestConfig.getElementByName(driver, 'App Run View');
 
     const codingArea = await TestConfig.getElementById(driver, 'coding-area');
-    const switchBtn = await TestConfig.getElementById(driver, 'switch');
+    const switchBtn = await TestConfig.getElementById(driver, 'switch-container');
     const startBtn = await TestConfig.getElementById(driver, 'start-button');
-    const stopBtn = await TestConfig.getElementById(driver, 'stop-button');
-    const memorySection = await TestConfig.getElementById(driver, 'memory-section');
+    const resetBtn = await TestConfig.getElementById(driver, 'reset-button');
+    const memorySection = await TestConfig.getElementByXpath(driver, '//*[@id="computer-column"]/div[1]');
 
     await appRunViewBtn.click();
     await appRunViewBtn.click();
@@ -57,7 +55,7 @@ describe('test app run view', () => {
     expect(await codingArea.isDisplayed()).toEqual(true);
     expect(await switchBtn.isDisplayed()).toEqual(true);
     expect(await startBtn.isDisplayed()).toEqual(true);
-    expect(await stopBtn.isDisplayed()).toEqual(true);
+    expect(await resetBtn.isDisplayed()).toEqual(true);
     expect(await memorySection.isDisplayed()).toEqual(true);
   }, 35000);
 });
