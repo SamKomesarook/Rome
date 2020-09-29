00e3c01b7e8ae39ca06a4d566a41efd14814d5a8
sherryysj <45263452+sherryysj@users.noreply.github.com>
2020-09-29 10:49:35 +1000

    add csv file source code, update UI

--- src/components/elements/Feedback.js
+++ src/components/elements/Feedback.js
@@ -10,18 +10,47 @@ const Feedback = ({ isActive }) => {
 
   const isActiveClass = isActive ? '' : ' hidden';
   
+  const submitFeedback = (e) => {
+	  console.log("hello");
+      const createCsvWriter = require('csv-writer').createArrayCsvWriter;
+      const csvWriter = createCsvWriter({
+          header: ['NAME', 'LANGUAGE'],
+          path: './file.csv'
+      });
+
+      const records = [
+          ['Bob', 'French, English'],
+          ['Mary', 'English']
+        ];
+		
+/*bug here
+      csvWriter.writeRecords(records)// returns a promise
+          .then(() => {
+             console.log('...Done');
+       });
+*/  
+  }
+  
   
   return (
     <div id="feedback" className={`sidebar-item-pane${isActiveClass}`}>
-      <div className="control-container">
-	      Email: <input id="emailInput" type="text" placeholder="input your email" className="control"/>
-	  </div>
-	  <div className="control-container">
-	      Feedback: <input id="feedbackInput" type="text" placeholder="input your feedback for us" className="control"/>
-      </div>
-	  <div id="feedback-content">
-         12345
-      </div>
+	  <form id="input-control">
+	    <div className="control-container">
+          <span style={{color:"white"}}>Email:</span>
+          <input type="email" id="feedbackEmail" name="feedbackEmail" placeholder="input your email" className="control" />
+        </div>
+		<div className="control-container">
+		  <div>
+		    <span style={{color:"white"}}>Feedback:</span>
+          </div>
+		  <div>
+		    <textArea type="text" form="input-control" id="feedbackContent" name="feedbackContent" placeholder="input your feedback for us" />
+          </div>
+		</div>
+		<div id="feedbackSubmit">
+		  <input id="feedbackButton" type="submit" value="Submit" onClick={submitFeedback("this.form")}/>
+        </div>  
+	  </form> 
     </div>
   );
 };
