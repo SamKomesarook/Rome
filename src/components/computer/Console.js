cc636b3f4061f4f1be7896ba11b533ff558d636c
Ton Binh On <nick.on437@gmail.com>
2020-10-02 23:22:47 +1000

    Refactor - Code cleaning

--- src/components/computer/Console.js
+++ src/components/computer/Console.js
@@ -1,4 +1,4 @@
-import React, { useContext, useRef, useEffect } from 'react';
+import React, { useContext, useRef } from 'react';
 import { TerminalNodeImpl } from 'antlr4/tree/Tree';
 import antlr4 from 'antlr4';
 import { DisplayContext } from '../../state/DisplayState';
@@ -9,78 +9,8 @@ import { RomeParser } from '../../lang/grammar/Rome/RomeParser';
 import { MachineLexer } from '../../lang/grammar/Machine/MachineLexer';
 import { MachineParser } from '../../lang/grammar/Machine/MachineParser';
 
-// const Console = () => {
-//   const [display, setDisplay] = useContext(DisplayContext);
-
-//   const handleKey = (event) => {
-//     if (event.keyCode === 13) {
-//       // Create a deep copy of display
-//       const staticDisplay = DisplayContext.createCustomClone(display);
-//       const newMem = staticDisplay.memory;
-
-//       // Get the keys of special memory cells
-//       const usbMemoryKey = staticDisplay.specialKeys.find((element) => element.specialContent === 'usb').key;
-//       if (staticDisplay.selected === usbMemoryKey) {
-//         USBToggle();
-//       } else {
-//         newMem[staticDisplay.selected].content = staticDisplay.input;
-//         newMem[staticDisplay.selected].type = 'string';
-//       }
-//       staticDisplay.memory = newMem;
-//       staticDisplay.reading = false;
-//       processInstrs(staticDisplay);
-
-//       // Render new display information
-//       setDisplay(DisplayContext.createCustomClone(staticDisplay));
-//     }
-//   };
-
-//   const handleChange = (event) => {
-//     event.preventDefault();
-//     const { value } = event.target;
-//     setDisplay((prevDisplay) => ({
-//       ...prevDisplay,
-//       input: value,
-//     }));
-//   };
-
-//   return (
-//     <input
-//       id="input-bar"
-//       size="60"
-//       type="text"
-//       onKeyDown={handleKey}
-//       onChange={handleChange}
-//       disabled={!display.reading}
-//       value={!display.reading ? '' : display.input}
-//     />
-//   );
-// };
-
-// const OutputArea = () => {
-//   const [display] = useContext(DisplayContext);
-//   return (
-//     <div
-//       contentEditable="false"
-//       id="output-area"
-//       style={{
-//         'background-color': `${display.outputStyle.bgColor}`,
-//         color: `${display.outputStyle.txtColor}`,
-//         'font-size': `${display.outputStyle.txtSize}`,
-//         'text-align': `${display.outputStyle.txtAlign}`,
-//         'font-weight': `${display.outputStyle.bold}`,
-//         'font-style': `${display.outputStyle.italic}`,
-//         'text-decoration-line': `${display.outputStyle.underline}`,
-//       }}
-//     >
-//       {display.output}
-//     </div>
-//   );
-// };
-
 const Console = () => {
   const [display, setDisplay] = useContext(DisplayContext);
-  const [consoleHistory, setConsoleHistory] = useContext(DisplayContext);
   const inputRef = useRef();
 
   const executeStart = () => {
