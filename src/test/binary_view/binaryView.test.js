e39ead862d67556f37b073ef1c13e64be182a408
czzlyin <62701549+czzlyin@users.noreply.github.com>
2020-10-02 14:43:11 +1000

    refactor the test of binaryView.js
    refactor with more reasonable test method

--- src/test/binary_view/binaryView.test.js
+++ src/test/binary_view/binaryView.test.js
@@ -4,6 +4,7 @@ const webdriver = require('selenium-webdriver');
 
 const testBinaryViewOfInteger = 'test the binary view of integer type';
 const testBinaryViewOfLong = 'test the binary view of long type';
+const testBinaryViewOfFloat = 'test the binary view of float type';
 const testBinaryViewOfNegative = 'test the binary view of negative number';
 const testBinaryViewOfCharacter = 'test the binary view of character type';
 const testBinaryViewOfString = 'test the binary view of string type';
@@ -25,9 +26,37 @@ describe('test binary view', () => {
     await driver.navigate().refresh();
   }, 30000);
 
+  function listNum(str) {
+    const strArray = str.split('');
+    let res = '';
+    for (let i = 0; i < str.length - 1; i++) {
+      res = `${res + strArray[i]}\n`;
+    }
+    res += strArray[strArray.length - 1];
+    return res;
+  }
+
+  function fillIn(str) {
+    let fill = '';
+    for (let i = 0; i < 8 - str.length; i++) {
+      fill += '0';
+    }
+    return fill + str;
+  }
+
+  function transfer(str) {
+    let res = '';
+    for (let i = 0; i < 6; i++) {
+      res += fillIn(str.charCodeAt(i).toString(2));
+    }
+    return res;
+  }
+
   test(testBinaryViewOfInteger, async () => {
+    const value = 43697;
+    const str = value.toString();
     const codingArea = await TestConfig.getElementById(driver, 'coding-area');
-    await codingArea.sendKeys('start\nset(integer)\nwrite(43697)\nend');
+    await codingArea.sendKeys(`start\nset(integer)\nwrite(${str})\nend`);
 
     const startBtn = await TestConfig.getElementById(driver, 'start-button');
     await startBtn.click();
@@ -37,7 +66,9 @@ describe('test binary view', () => {
 
     const binaryCells = await TestConfig.getElementById(driver, 'memory-0');
     const binaryCellsRes = await binaryCells.getText();
-    expect(binaryCellsRes).toEqual('1\n0\n1\n0\n1\n0\n1\n0\n1\n0\n1\n1\n0\n0\n0\n1');
+    const bValue = parseInt(value).toString(2);
+    const res = listNum(bValue);
+    expect(binaryCellsRes).toEqual(res);
 
     const outputArea = await TestConfig.getElementById(driver, 'output-area');
     const outputAreaRes = await outputArea.getText();
@@ -46,8 +77,10 @@ describe('test binary view', () => {
   }, 35000);
 
   test(testBinaryViewOfLong, async () => {
+    const value = 4163953387;
+    const str = value.toString();
     const codingArea = await TestConfig.getElementById(driver, 'coding-area');
-    await codingArea.sendKeys('start\nset(long)\nwrite(655535)\nend');
+    await codingArea.sendKeys(`start\nset(long)\nwrite(${str})\nend`);
 
     const startBtn = await TestConfig.getElementById(driver, 'start-button');
     await startBtn.click();
@@ -57,7 +90,37 @@ describe('test binary view', () => {
 
     const binaryCells = await TestConfig.getElementById(driver, 'memory-0');
     const binaryCellsRes = await binaryCells.getText();
-    expect(binaryCellsRes).toEqual('0\n0\n0\n0\n0\n0\n0\n0\n0\n0\n0\n0\n1\n0\n1\n0\n0\n0\n0\n0\n0\n0\n0\n0\n1\n0\n1\n0\n1\n1\n1\n1');
+    const bValue = parseInt(value).toString(2);
+    const res = listNum(bValue);
+    expect(binaryCellsRes).toEqual(res);
+
+    const outputArea = await TestConfig.getElementById(driver, 'output-area');
+    const outputAreaRes = await outputArea.getText();
+
+    expect(outputAreaRes).toEqual('');
+  }, 35000);
+
+  test(testBinaryViewOfFloat, async () => {
+    const codingArea = await TestConfig.getElementById(driver, 'coding-area');
+    await codingArea.sendKeys('start\nset(float)\nwrite(10.75)\nmove(next)\nset(float)\nwrite(-10.75)\nend');
+
+    const startBtn = await TestConfig.getElementById(driver, 'start-button');
+    await startBtn.click();
+
+    const memoryCell0 = await TestConfig.getElementById(driver, 'memory-0');
+    await driver.actions().doubleClick(memoryCell0).perform();
+
+    const memoryCell1 = await TestConfig.getElementById(driver, 'memory-1');
+    await driver.actions().doubleClick(memoryCell1).perform();
+
+    const binaryCells0 = await TestConfig.getElementById(driver, 'memory-0');
+    const binaryCells0Res = await binaryCells0.getText();
+
+    const binaryCells1 = await TestConfig.getElementById(driver, 'memory-1');
+    const binaryCells1Res = await binaryCells1.getText();
+
+    expect(binaryCells0Res).toEqual(listNum('01000001001011000000000000000000'));
+    expect(binaryCells1Res).toEqual(listNum('11000001001011000000000000000000'));
 
     const outputArea = await TestConfig.getElementById(driver, 'output-area');
     const outputAreaRes = await outputArea.getText();
@@ -66,8 +129,11 @@ describe('test binary view', () => {
   }, 35000);
 
   test(testBinaryViewOfNegative, async () => {
+    const value = -43697;
+    const absValue = 43697;
+    const str = value.toString();
     const codingArea = await TestConfig.getElementById(driver, 'coding-area');
-    await codingArea.sendKeys('start\nset(integer)\nwrite(-43967)\nend');
+    await codingArea.sendKeys(`start\nset(integer)\nwrite(${str})\nend`);
 
     const startBtn = await TestConfig.getElementById(driver, 'start-button');
     await startBtn.click();
@@ -77,7 +143,10 @@ describe('test binary view', () => {
 
     const binaryCells = await TestConfig.getElementById(driver, 'memory-0');
     const binaryCellsRes = await binaryCells.getText();
-    expect(binaryCellsRes).toEqual('0\n1\n0\n1\n0\n1\n0\n0\n0\n1\n0\n0\n0\n0\n0\n0');
+    const bValue = parseInt(absValue).toString(2);
+    const negBin = bValue.replace(/1/g, '9').replace(/0/g, '1').replace(/9/g, '0');
+    const res = listNum(negBin);
+    expect(binaryCellsRes).toEqual(res);
 
     const outputArea = await TestConfig.getElementById(driver, 'output-area');
     const outputAreaRes = await outputArea.getText();
@@ -97,7 +166,9 @@ describe('test binary view', () => {
 
     const binaryCells = await TestConfig.getElementById(driver, 'memory-0');
     const binaryCellsRes = await binaryCells.getText();
-    expect(binaryCellsRes).toEqual('0\n1\n1\n1\n0\n0\n1\n0');
+    const bValue = fillIn('r'.charCodeAt(0).toString(2));
+    const res = listNum(bValue);
+    expect(binaryCellsRes).toEqual(res);
 
     const outputArea = await TestConfig.getElementById(driver, 'output-area');
     const outputAreaRes = await outputArea.getText();
@@ -106,6 +177,8 @@ describe('test binary view', () => {
   }, 35000);
 
   test(testBinaryViewOfString, async () => {
+    const str1 = 'romela';
+    const str2 = 'nguage';
     const codingArea = await TestConfig.getElementById(driver, 'coding-area');
     await codingArea.sendKeys('start\nset(string)\nwrite("romelanguage")\nend');
 
@@ -120,12 +193,16 @@ describe('test binary view', () => {
 
     const binaryCells0 = await TestConfig.getElementById(driver, 'memory-0');
     const binaryCells0Res = await binaryCells0.getText();
+    const bValue0 = transfer(str1);
+    const res0 = listNum(bValue0);
 
     const binaryCells1 = await TestConfig.getElementById(driver, 'memory-1');
     const binaryCells1Res = await binaryCells1.getText();
+    const bValue1 = transfer(str2);
+    const res1 = listNum(bValue1);
 
-    expect(binaryCells0Res).toEqual('0\n1\n1\n1\n0\n0\n1\n0\n0\n1\n1\n0\n1\n1\n1\n1\n0\n1\n1\n0\n1\n1\n0\n1\n0\n1\n1\n0\n0\n1\n0\n1\n0\n1\n1\n0\n1\n1\n0\n0\n0\n1\n1\n0\n0\n0\n0\n1');
-    expect(binaryCells1Res).toEqual('0\n1\n1\n0\n1\n1\n1\n0\n0\n1\n1\n0\n0\n1\n1\n1\n0\n1\n1\n1\n0\n1\n0\n1\n0\n1\n1\n0\n0\n0\n0\n1\n0\n1\n1\n0\n0\n1\n1\n1\n0\n1\n1\n0\n0\n1\n0\n1');
+    expect(binaryCells0Res).toEqual(res0);
+    expect(binaryCells1Res).toEqual(res1);
 
     const outputArea = await TestConfig.getElementById(driver, 'output-area');
     const outputAreaRes = await outputArea.getText();
