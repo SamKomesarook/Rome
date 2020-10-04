0b1daa1850248e7ff65c978a49a1a66197d77550
czzlyin <62701549+czzlyin@users.noreply.github.com>
2020-10-04 23:16:55 +1100

    remove debug button from sibebar
    remove debug button from sibebar

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -46,14 +46,6 @@ const Sidebar = () => {
     }));
   };
 
-  const handleDebugMode = () => {
-    setActiveSidebarItem('');
-    setUi((prevUI) => ({
-      ...prevUI,
-      ctxIsDebugActive: !prevUI.ctxIsDebugActive,
-    }));
-  };
-
   const sidebarItemList = SIDEBAR_ITEMS.map((item) => (
     <SidebarButton
       key={item.name}
@@ -69,15 +61,6 @@ const Sidebar = () => {
       <ul className="sidebar-item-list">
         { sidebarItemList }
         <li className="sidebar-item">
-          <button
-            type="button"
-            className="sidebar-btn toggle-btn"
-            onClick={handleDebugMode}
-            name="Debug"
-          >
-            <span className="sidebar-item-text">Debug Mode</span>
-            <svg viewBox="0 0 24 24"><path fill="currentColor" d="M9,6H5V10H7V8H9M19,10H17V12H15V14H19M21,16H3V4H21M21,2H3C1.89,2 1,2.89 1,4V16A2,2 0 0,0 3,18H10V20H8V22H16V20H14V18H21A2,2 0 0,0 23,16V4C23,2.89 22.1,2 21,2" /></svg>
-          </button>
           <button
             type="button"
             className="sidebar-btn toggle-btn"
