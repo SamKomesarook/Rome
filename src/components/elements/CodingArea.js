a92458a35e594502ec22d61b79dc0acb0281c39f
Nick On <nick,on437@gmail.com>
2020-10-21 00:53:09 +1100

    New - Add custom type effect and create code coloring layer

--- src/components/elements/CodingArea.js
+++ src/components/elements/CodingArea.js
@@ -1,9 +1,26 @@
-import React, { useContext } from 'react';
-import Typed from 'react-typed';
+import React, { useContext, useRef } from 'react';
 import { DisplayContext } from '../../state/DisplayState';
+import TypeWriter from '../../utils/TypeWriter';
 
 const CodingArea = () => {
   const [display, setDisplay] = useContext(DisplayContext);
+  const codingAreaRef = useRef();
+  const sample = [
+    'start\nmove(next)\nmove(last)\nend',
+    'start\nset(letters)\nwrite("hello!")\nend',
+    'start\nset(numbers)\nwrite(4)\nend',
+    'start\nset(letters)\nwrite("hello")\nfree\nwrite("world!")\nend',
+    'start\nloop(3){\nset(letters)\nwrite("content")\nmove(next)\n}\nend',
+    'start\nset(numbers)\nwrite(3)\nmove(next)\nloop(memory(1)){\nset(letters)\nwrite("content")\nmove(next)\n}\nend',
+    'start\nset(numbers)\nwrite(3)\nname("first")\nmove(next)\nloop(memory("first")){\nset(letters)\nwrite("content")\nmove(next)\n}\nend',
+    'start\nset(numbers)\nwrite(3)\nif(is equal 3){\nmove(next)\nset(letters)\nwrite("is equal to 3!")\n}\nend',
+    'start\nset(numbers)\nwrite(3)\nif(not less 3){\nmove(next)\nset(letters)\nwrite("is not less than 3!")\n}\nend',
+    'start\nset(numbers)\nwrite(3)\nif(is greater 3){\nmove(next)\nset(letters)\nwrite("is greater than 3!")\n}\nend',
+    'start\nimport(network)\nn_write("hello!")\nend',
+    'start\nimport(network)\nn_read\nend',
+    'start\nimport(IO)\nk_read\nend',
+    'start\nimport(IO)\nk_write("hello!")\nend',
+  ];
 
   function handleChange(event) {
     event.preventDefault();
@@ -15,31 +32,23 @@ const CodingArea = () => {
     }));
   }
 
+  // Init placeholder on DOM load
+  document.addEventListener('DOMContentLoaded', () => new TypeWriter(codingAreaRef.current, sample));
+
   return (
-    <div id="coding-area-wrapper" className="code">
-      <Typed
-        strings={[
-          'start\nmove(next)\nmove(last)\nend',
-          'start\nset(string)\nwrite("hello!")\nend',
-          'start\nset(integer)\nwrite(4)\nend',
-          'start\nset(string)\nwrite("hello")\nfree\nwrite("world!")\nend',
-          'start\nloop(3){\nset(string)\nwrite("content")\nmove(next)\n}\nend',
-          'start\nset(integer)\nwrite(3)\nmove(next)\nloop(memory(1)){\nset(string)\nwrite("content")\nmove(next)\n}\nend',
-          'start\nset(integer)\nwrite(3)\nname("first")\nmove(next)\nloop(memory("first")){\nset(string)\nwrite("content")\nmove(next)\n}\nend',
-          'start\nset(integer)\nwrite(3)\nif(is equal 3){\nmove(next)\nset(string)\nwrite("is equal to 3!")\n}\nend',
-          'start\nset(integer)\nwrite(3)\nif(not less 3){\nmove(next)\nset(string)\nwrite("is not less than 3!")\n}\nend',
-          'start\nset(integer)\nwrite(3)\nif(is greater 3){\nmove(next)\nset(string)\nwrite("is greater than 3!")\n}\nend',
-          'start\nimport(IO)\nkeyboardRead\nend',
-          'start\nimport(IO)\nconsoleWrite("hello!")\nend',
-        ]}
-        typeSpeed={40}
-        backSpeed={30}
-        attr="placeholder"
-        loop
-        showCursor={false}
-      >
-        <textarea id="coding-area" className="highlightable-input" onChange={handleChange.bind(this)} spellCheck={false} />
-      </Typed>
+    <div id="coding-area-wrapper" className="code highlightable-input">
+      <div
+        id="coding-area-color-layer"
+      />
+      <div
+        id="coding-area"
+        ref={codingAreaRef}
+        onInput={handleChange}
+        // className="highlightable-input"
+        spellCheck={false}
+        contentEditable="true"
+        data-placeholder=""
+      />
     </div>
   );
 };
