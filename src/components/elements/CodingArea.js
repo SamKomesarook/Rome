b258c541c730c5fa69af3f3e5d91f576ac785b06
Nick On <nick,on437@gmail.com>
2020-11-02 23:08:55 +1100

    Improve - Save and load code automatically on load

--- src/components/elements/CodingArea.js
+++ src/components/elements/CodingArea.js
@@ -1,10 +1,12 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useRef, useEffect } from 'react';
 import Typed from 'react-typed';
 import Cookies from 'js-cookie';
 import { DisplayContext } from '../../state/DisplayState';
 
 const CodingArea = () => {
   const [display, setDisplay] = useContext(DisplayContext);
+  const codingAreaWrapperRef = useRef(null);
+
   function handleChange(event) {
     event.preventDefault();
     const { value } = event.target;
@@ -16,8 +18,15 @@ const CodingArea = () => {
     Cookies.set('history', value, { expires: 3 });
   }
 
+  useEffect(() => {
+    // Because of Typed library, CodingAreaRef cannot be assigned directly, thus, this workaround
+    const codingArea = codingAreaWrapperRef.current.children[0].children[0];
+    const savedCode = Cookies.get('history');
+    codingArea.value = savedCode;
+  }, [codingAreaWrapperRef]);
+
   return (
-    <div id="coding-area-wrapper" className="code">
+    <div ref={codingAreaWrapperRef} id="coding-area-wrapper" className="code">
       <Typed
         strings={['start\nmove(next)\nmove(last)\nend',
           'start\nset(string)\nwrite("hello!")\nend',
