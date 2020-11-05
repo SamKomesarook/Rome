790b579356c7d6aa2be53602a1062a31782a5298
sk <skomesarook@protonmail.com>
2020-11-05 13:53:04 +1100

    removed typed

--- src/components/elements/CodingArea.js
+++ src/components/elements/CodingArea.js
@@ -17,29 +17,7 @@ const CodingArea = () => {
 
   return (
     <div id="coding-area-wrapper" className="code highlightable-input">
-      <Typed
-        strings={[
-          'start\nmove(next)\nmove(last)\nend',
-          'start\nset(string)\nwrite("hello!")\nend',
-          'start\nset(integer)\nwrite(4)\nend',
-          'start\nset(string)\nwrite("hello")\nfree\nwrite("world!")\nend',
-          'start\nloop(3){\nset(string)\nwrite("content")\nmove(next)\n}\nend',
-          'start\nset(integer)\nwrite(3)\nmove(next)\nloop(memory(1)){\nset(string)\nwrite("content")\nmove(next)\n}\nend',
-          'start\nset(integer)\nwrite(3)\nname("first")\nmove(next)\nloop(memory("first")){\nset(string)\nwrite("content")\nmove(next)\n}\nend',
-          'start\nset(integer)\nwrite(3)\nif(is equal 3){\nmove(next)\nset(string)\nwrite("is equal to 3!")\n}\nend',
-          'start\nset(integer)\nwrite(3)\nif(not less 3){\nmove(next)\nset(string)\nwrite("is not less than 3!")\n}\nend',
-          'start\nset(integer)\nwrite(3)\nif(is greater 3){\nmove(next)\nset(string)\nwrite("is greater than 3!")\n}\nend',
-          'start\nimport(IO)\nkeyboardRead\nend',
-          'start\nimport(IO)\nconsoleWrite("hello!")\nend',
-        ]}
-        typeSpeed={40}
-        backSpeed={30}
-        attr="placeholder"
-        loop
-        showCursor={false}
-      >
-        <textarea id="coding-area" onChange={handleChange.bind(this)} spellCheck={false} />
-      </Typed>
+      <textarea id="coding-area" onChange={handleChange.bind(this)} spellCheck={false} />
     </div>
   );
 };
